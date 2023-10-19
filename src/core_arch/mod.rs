9dc8aaa0373b8766a1242018b9d6344d38e8a5f4
sarah <>
2023-10-20 00:53:44 +0200

    implement aarch64 neon simd

--- src/core_arch/mod.rs
+++ src/core_arch/mod.rs
@@ -59,7 +59,7 @@ macro_rules! feature_detected {
 #[macro_export]
 macro_rules! feature_detected {
     ($feature: tt) => {
-        ::std::is_aarch64_feature_detected!($feature)
+        ::std::arch::is_aarch64_feature_detected!($feature)
     };
 }
 
@@ -132,6 +132,55 @@ macro_rules! __impl_type {
     ("adx") => { $crate::core_arch::x86::Adx };
     ("rtm") => { $crate::core_arch::x86::Rtm };
     ("abm") => { $crate::core_arch::x86::Abm };
+}
+
+#[cfg(target_arch = "aarch64")]
+#[doc(hidden)]
+#[rustfmt::skip]
+#[macro_export]
+macro_rules! __impl_type {
+    ("neon") => { $crate::core_arch::aarch64::Neon };
+    ("pmull") => { $crate::core_arch::aarch64::Pmull };
+    ("fp") => { $crate::core_arch::aarch64::Fp };
+    ("fp16") => { $crate::core_arch::aarch64::Fp16 };
+    ("sve") => { $crate::core_arch::aarch64::Sve };
+    ("crc") => { $crate::core_arch::aarch64::Crc };
+    ("lse") => { $crate::core_arch::aarch64::Lse };
+    ("lse2") => { $crate::core_arch::aarch64::Lse2 };
+    ("rdm") => { $crate::core_arch::aarch64::Rdm };
+    ("rcpc") => { $crate::core_arch::aarch64::Rcpc };
+    ("rcpc2") => { $crate::core_arch::aarch64::Rcpc2 };
+    ("dotprod") => { $crate::core_arch::aarch64::Dotprod };
+    ("tme") => { $crate::core_arch::aarch64::Tme };
+    ("fhm") => { $crate::core_arch::aarch64::Fhm };
+    ("dit") => { $crate::core_arch::aarch64::Dit };
+    ("flagm") => { $crate::core_arch::aarch64::Flagm };
+    ("ssbs") => { $crate::core_arch::aarch64::Ssbs };
+    ("sb") => { $crate::core_arch::aarch64::Sb };
+    ("paca") => { $crate::core_arch::aarch64::Paca };
+    ("pacg") => { $crate::core_arch::aarch64::Pacg };
+    ("dpb") => { $crate::core_arch::aarch64::Dpb };
+    ("dpb2") => { $crate::core_arch::aarch64::Dpb2 };
+    ("sve2") => { $crate::core_arch::aarch64::Sve2 };
+    ("sve2-aes") => { $crate::core_arch::aarch64::Sve2Aes };
+    ("sve2-sm4") => { $crate::core_arch::aarch64::Sve2Sm4 };
+    ("sve2-sha3") => { $crate::core_arch::aarch64::Sve2Sha3 };
+    ("sve2-bitperm") => { $crate::core_arch::aarch64::Sve2Bitperm };
+    ("frintts") => { $crate::core_arch::aarch64::Frintts };
+    ("i8mm") => { $crate::core_arch::aarch64::I8mm };
+    ("f32mm") => { $crate::core_arch::aarch64::F32mm };
+    ("f64mm") => { $crate::core_arch::aarch64::F64mm };
+    ("bf16") => { $crate::core_arch::aarch64::Bf16 };
+    ("rand") => { $crate::core_arch::aarch64::Rand };
+    ("bti") => { $crate::core_arch::aarch64::Bti };
+    ("mte") => { $crate::core_arch::aarch64::Mte };
+    ("jsconv") => { $crate::core_arch::aarch64::Jsconv };
+    ("fcma") => { $crate::core_arch::aarch64::Fcma };
+    ("aes") => { $crate::core_arch::aarch64::Aes };
+    ("sha2") => { $crate::core_arch::aarch64::Sha2 };
+    ("sha3") => { $crate::core_arch::aarch64::Sha3 };
+    ("sm4") => { $crate::core_arch::aarch64::Sm4 };
+    ("asimd") => { $crate::core_arch::aarch64::Asimd };
 }
 
 #[macro_export]
@@ -338,6 +387,10 @@ macro_rules! internal_simd_type {
 
 pub(crate) use internal_simd_type;
 
-#[cfg(any(doc, target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 #[cfg_attr(docsrs, doc(cfg(any(target_arch = "x86", target_arch = "x86_64"))))]
 pub mod x86;
+
+#[cfg(target_arch = "aarch64")]
+#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
+pub mod aarch64;
