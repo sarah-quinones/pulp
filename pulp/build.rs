eebfa31c43f88d064563a649219570d294385beb
sarah-quiñones <sarah@veganb.tw>
2024-12-13 23:18:12 +0100

    try fixing x86_64-apple-darwin

--- pulp/build.rs
+++ pulp/build.rs
@@ -199,14 +199,14 @@ fn main() {
 
 			if mask != 0 || !wrote_empty {
 				let ld = format!("libpulp_{ver}_ld_b32s_{mask:0>16b}");
-				f += &format!(".globl {ld}\n");
+				f += &format!(".global {ld}\n");
 				f += &format!("{ld}:\n");
 				f += &load_f32x16(start, end);
 				f += "jmp rcx\n";
 				names.push(ld);
 
 				let st = format!("libpulp_{ver}_st_b32s_{mask:0>16b}");
-				f += &format!(".globl {st}\n");
+				f += &format!(".global {st}\n");
 				f += &format!("{st}:\n");
 				f += &store_f32x16(start, end);
 				f += "jmp rcx\n";
